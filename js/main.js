e0d8824ddef54f1ce012933cefdfa0f0640f1e0c
Reo Yamanaka <ryamanaka807@gmail.com>
2021-02-14 00:06:40 -0500

    added CSS for showcase section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function navMod() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
