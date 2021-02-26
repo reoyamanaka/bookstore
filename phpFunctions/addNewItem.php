0a49b941c63ff5a1b122c85c2dd01a15b6b441b5
Reo Yamanaka <ryamanaka807@gmail.com>
2021-02-25 22:00:22 -0500

    started addNewItem function

---
+++ phpFunctions/addNewItem.php
@@ -0,0 +1,17 @@
+<?php
+  if (isset($_POST['uploadNew'])){
+    $file = $_FILES['file'];
+    $fileName = $_FILES['file']['name'];
+    $fileTmpName = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileError = $_FILES['file']['error'];
+    $fileType = $_FILES['file']['type'];
+
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+
+    $allowed = array('jpg', 'jpeg', 'png');
+
+  }
+
+ ?>
