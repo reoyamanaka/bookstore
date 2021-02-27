e2faeb257078fa5ce3193a792a9512b3813da5f4
Reo Yamanaka <ryamanaka807@gmail.com>
2021-02-26 23:25:16 -0500

    completed first version of add new item function

--- phpFunctions/addNewItem.php
+++ phpFunctions/addNewItem.php
@@ -12,6 +12,49 @@
 
     $allowed = array('jpg', 'jpeg', 'png');
 
+    if (in_array($fileActualExt, $allowed)) {
+      if ($fileError === 0) {
+        if ($fileSize < 5000000) {
+          $fileNameNew = uniqid('', true).".".$fileActualExt;
+          $fileDestination = 'images/'.$fileNameNew;
+          move_uploaded_file($fileTmpName, $fileDestination);
+
+          $newTitle = $_POST['newTitle'];
+          $newAuthor = $_POST['newAuthor'];
+          $newPrice = $_POST['newPrice'];
+          $newGenre = $_POST['newGenre'];
+          $newRating = $_POST['newRating'];
+
+          $conn = new mysqli("localhost", "your_username", "your_password", "bookstore") or die(mysqli_error($conn));
+
+
+          $conn->query("ALTER TABLE inventory MODIFY id integer");
+          $conn->query("ALTER TABLE inventory DROP PRIMARY KEY");
+          $conn->query("UPDATE inventory SET id = 0");
+          $conn->query("ALTER TABLE inventory MODIFY id int");
+          $conn->query("ALTER TABLE inventory MODIFY COLUMN id int NOT NULL Primary Key auto_increment First");
+
+          $stmt = $conn->prepare("INSERT INTO inventory (title, author, price, image, rating, genre) VALUES(?,?,?,?,?,?)");
+          $stmt->bind_param('ssssss', $newTitle, $newAuthor, $newPrice, $fileNameNew, $newRating, $newGenre);
+          $stmt->execute();
+
+          $conn->query("ALTER TABLE inventory MODIFY id integer");
+          $conn->query("ALTER TABLE inventory DROP PRIMARY KEY");
+          $conn->query("UPDATE inventory SET id = 0");
+          $conn->query("ALTER TABLE inventory MODIFY id int");
+          $conn->query("ALTER TABLE inventory MODIFY COLUMN id int NOT NULL Primary Key auto_increment First");
+
+          header("Location: index.php#new");
+        } else {
+          echo "Your file is too large.";
+        }
+      } else {
+        echo "There was an error uploading your file.";
+      }
+    } else {
+      echo "You cannot upload files of this type.";
+    }
+
   }
 
  ?>
